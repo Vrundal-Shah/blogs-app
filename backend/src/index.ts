c7a3fcbb4409b83ed4572d2fe83ac163dfc1f786
vrundal-shah <vrundalshah05@gmail.com>
2023-12-19 09:55:22 -0800

    change back frontebd link to render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,7 +36,7 @@ interface ContextType {
 const isProduction = process.env.NODE_ENV === 'production';
 
 const allowedOrigin = isProduction
-  ? 'https://blogs-app-vrundal-shah.vercel.app' // Replace with your actual production domain
+  ? 'https://blogspot-63t1.onrender.com' // Replace with your actual production domain
   : 'http://localhost:3001';
 
 async function createContext({
